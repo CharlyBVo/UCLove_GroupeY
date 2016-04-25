d0e89f3ee3f16f9173edac0b2c925d3d936c5369
CharlyBVo <charles-henry.bertrand@student.uclouvain.be>
2016-04-25 11:00:14 +0200

    Update UtilisateurManager.java

--- Activities/UtilisateurManager.java
+++ Activities/UtilisateurManager.java
@@ -8,6 +8,8 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class UtilisateurManager {
@@ -62,7 +64,6 @@ public class UtilisateurManager {
 
         ContentValues values = new ContentValues();
         values.put(KEY_CHEVEUX_UTILISATEUR, util.getCheveux());
-        //values.put(KEY_DATE_NAISSANCE_UTILISATEUR,util.getDateDeNaissance());
         values.put(KEY_GENRE_UTILISATEUR,util.getGenre());
         values.put(KEY_LANGUE_UTILISATEUR,util.getLangue());
         values.put(KEY_LOGIN_UTILISATEUR,util.getLogin());
@@ -75,10 +76,15 @@ public class UtilisateurManager {
         values.put(KEY_TELEPHONE_UTILISATEUR,util.getTelephone());
         //values.put(KEY_PHOTO_UTILISATEUR,null);
         values.put(KEY_YEUX_UTILISATEUR, util.getYeux());
+
+        //Gestion de la date de naissance
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String dateString = sdf.format(util.getDateDeNaissance());
+
+        values.put(KEY_DATE_NAISSANCE_UTILISATEUR, dateString);
+
         long result=db.insert(TABLE_NAME,null,values);
         db.execSQL("UPDATE "+TABLE_NAME+" SET "+KEY_PHOTO_UTILISATEUR+" ='"+util.getPhoto()+"' WHERE "
-                +KEY_LOGIN_UTILISATEUR+"='"+util.getLogin()+"' ;");
-        db.execSQL("UPDATE "+TABLE_NAME+" SET "+KEY_DATE_NAISSANCE_UTILISATEUR+" ='"+util.getDateDeNaissance()+"' WHERE "
                 +KEY_LOGIN_UTILISATEUR+"='"+util.getLogin()+"' ;");
 
 
@@ -108,19 +114,40 @@ public class UtilisateurManager {
 
         return db.delete(TABLE_NAME, where, whereArgs);
     }
+    public ArrayList<String> getFriendsofUser(String log)
+    {
+        //retourne un tableau contenant les logins des utilisateurs amis
+        RelationManager relM= new RelationManager();
+        ArrayList<String> Friends = new ArrayList<String>();
+        Cursor c = db.rawQuery("SELECT " + KEY_LOGIN_UTILISATEUR + " FROM " + TABLE_NAME + " U, " + relM.TABLE_NAME +
+                " R WHERE ( (U." + KEY_LOGIN_UTILISATEUR + " = R." + relM.KEY_LOGIN2_RELATION + " and R." + relM.KEY_LOGIN1_RELATION + " = '" + log +
+                "') or (U." + KEY_LOGIN_UTILISATEUR + "= R." + relM.KEY_LOGIN1_RELATION + " and R." + relM.KEY_LOGIN2_RELATION + "= '" + log +
+                "')) and (R." + relM.KEY_TYPE_DE_RELATION_RELATION + " = 'Amis');", null);
+        if(c.moveToFirst())
+        {
+            int count= c.getCount();
+            for(int i=0;i<count;i++)
+            {
+                Friends.add(c.getString(c.getColumnIndex(KEY_LOGIN_UTILISATEUR)));
+                c.moveToNext();
+            }
+            return Friends;
+        }
+        return null;
+
+    }
 
     public Utilisateur getMainUtilisateur(String logTest) {
-        // Retourne l'animal dont l'id est passé en paramètre
+        // Retourne l'utilisateur dont l'id est passé en paramètre
 
         Utilisateur a=new Utilisateur();
 
         Cursor c = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " + KEY_LOGIN_UTILISATEUR + "= '" + logTest + "'", null);
-        //Date naissance =db.execSQL("SELECT "+KEY_DATE_NAISSANCE_UTILISATEUR+" FROM "+TABLE_NAME+" WHERE "+KEY_LOGIN_UTILISATEUR+"= '"+logTest+"'");
         if (c.moveToFirst()) {
             a.setLogin(c.getString(c.getColumnIndex(KEY_LOGIN_UTILISATEUR)));
             a.setNom(c.getString(c.getColumnIndex(KEY_NOM_UTILISATEUR)));
             a.setCheveux(c.getString(c.getColumnIndex(KEY_CHEVEUX_UTILISATEUR)));
-            //a.setDateDeNaissance(c.getString(c.getColumnIndex(KEY_DATE_NAISSANCE_UTILISATEUR)));
+            String dateNoConvert = c.getString(c.getColumnIndex(KEY_DATE_NAISSANCE_UTILISATEUR));
             a.setGenre(c.getString(c.getColumnIndex(KEY_GENRE_UTILISATEUR)));
             a.setPrenom(c.getString(c.getColumnIndex(KEY_PRENOM_UTILISATEUR)));
             a.setYeux(c.getString(c.getColumnIndex(KEY_YEUX_UTILISATEUR)));
@@ -129,16 +156,74 @@ public class UtilisateurManager {
             a.setVille(c.getString(c.getColumnIndex(KEY_VILLE_UTILISATEUR)));
             a.setOrientation(c.getString(c.getColumnIndex(KEY_ORIENTATION_UTILISATEUR)));
             a.setTelephone(c.getString(c.getColumnIndex(KEY_TELEPHONE_UTILISATEUR)));
-           // a.setPhoto(Hibernate.createBlob(c.getBlob(c.getColumnIndex(KEY_PHOTO_UTILISATEUR))));
+            //a.setPhoto(c.getType(c.getColumnIndex(KEY_PHOTO_UTILISATEUR)));
             a.setLangue(c.getString(c.getColumnIndex(KEY_LANGUE_UTILISATEUR)));
             c.close();
+            if(dateNoConvert!=null)
+            {
+                try {
+                   Date temp = new SimpleDateFormat("yyyy-MM-dd").parse(dateNoConvert);
+                   // Date temp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dateNoConvert);
+                    a.setDateDeNaissance(temp);
+                }
+                catch (Exception e)
+                {}
+            }
+            PreferenceVisibiliteManager prefMan = new PreferenceVisibiliteManager();
+            PreferenceVisibilite pref = new PreferenceVisibilite(logTest);
+
+            Cursor euh = db.rawQuery("SELECT * FROM " + prefMan.TABLE_NAME + " WHERE " + prefMan.KEY_LOGIN_PREFERENCE_DE_VISIBILITE + "= '" + logTest + "'", null);
+            if (euh.moveToFirst()) {
+                pref.setParametre_cheveux(euh.getInt(euh.getColumnIndex(prefMan.KEY_PARAMETRE_CHEVEUX_PREFERENCE_DE_VISIBILITE)));
+                pref.setParametre_disponibilite(euh.getInt(euh.getColumnIndex(prefMan.KEY_PARAMETRE_DISPONIBILITE_PREFERENCE_DE_VISIBILITE)));
+                pref.setParametre_mail(euh.getInt(euh.getColumnIndex(prefMan.KEY_PARAMETRE_MAIL_PREFERENCE_DE_VISIBILITE)));
+                pref.setParametre_nom(euh.getInt(euh.getColumnIndex(prefMan.KEY_PARAMETRE_NOM_PREFERENCE_DE_VISIBILITE)));
+                pref.setParametre_orientation(euh.getInt(euh.getColumnIndex(prefMan.KEY_PARAMETRE_ORIENTATION_PREFERENCE_DE_VISIBILITE)));
+                pref.setParametre_photo(euh.getInt(euh.getColumnIndex(prefMan.KEY_PARAMETRE_PHOTO_PREFERENCE_DE_VISIBILITE)));
+                pref.setParametre_telephone(euh.getInt(euh.getColumnIndex(prefMan.KEY_PARAMETRE_TELEPHONE_PREFERENCE_DE_VISIBILITE)));
+                pref.setParametre_yeux(euh.getInt(euh.getColumnIndex(prefMan.KEY_PARAMETRE_YEUX_PREFERENCE_DE_VISIBILITE)));
+                pref.setParametre_ville(euh.getInt(euh.getColumnIndex(prefMan.KEY_PARAMETRE_VILLE_PREFERENCE_DE_VISIBILITE)));
+                euh.close();
+            }
+            a.setMyVisibility(pref);
+
+
             return a;
         }
         else
             return null;
 
-
     }
+
+    public  Utilisateur getInfoUser (String logUser,int valueOfPref)
+    {
+        // Retourne un Utilisateur qui contient uniquement les donnees correspondant a la valeur de preference
+        // de visibilite passe en parametre (ou superieur).
+
+        Utilisateur a = this.getMainUtilisateur(logUser);
+        PreferenceVisibilite pref = a.getMyVisibility();
+        if(valueOfPref!=0)
+        {a.setMotDePasse(null);}
+        if(pref.getParametre_orientation()<valueOfPref)
+        {a.setOrientation(null);}
+        if(pref.getParametre_nom()<valueOfPref)
+        {a.setNom(null);}
+        if(pref.getParametre_yeux()<valueOfPref)
+        {a.setYeux(null);}
+        if(pref.getParametre_cheveux()<valueOfPref)
+        {a.setCheveux(null);}
+        if(pref.getParametre_telephone()<valueOfPref)
+        {a.setTelephone(null);}
+        if(pref.getParametre_orientation()<valueOfPref)
+        {a.setOrientation(null);}
+        if(pref.getParametre_mail()<valueOfPref)
+        {a.setMail(null);}
+        if(pref.getParametre_photo()<valueOfPref)
+        {a.setPhoto(null);}
+        if(pref.getParametre_ville()<valueOfPref)
+        {a.setVille(null);}
+            return a;
+           }
 
     /*public Cursor getAnimaux() {
         // sélection de tous les enregistrements de la table
